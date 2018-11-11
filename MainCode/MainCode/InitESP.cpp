e5a177e8c17910b2c51b36cf639bbdd66fef3b8c
0ldmanB <davidstephenbarry@gmail.com>
2018-11-10 20:53:09 -0800

    All functions moved out of main, except for checkSystemState, which runs in the main loop.

--- MainCode/MainCode/InitESP.cpp
+++ MainCode/MainCode/InitESP.cpp
@@ -14,6 +14,7 @@
 #include "sys/time.h"
 #include "sdkconfig.h"
 #include "driver\adc.h"
+#include "GeneralFunctions.h"
 
 
 
@@ -55,6 +56,10 @@
 #define solarPanelVoltage A7
 
 int serialBaud = 115200;
+
+int getFlow(int column, int row, int turretColumn, int turretRow);
+int convertAngleToStep(double angle);
+double angleToSquare(int sqCol, int sqRow, int turCol, int turRow);
 
 void initESP()
 {
@@ -133,3 +138,187 @@ void initPins()
 	ledcWrite(stepperDomeCrntPin, 204);	//sets current limi of dome to ~500mA
 	ledcWrite(stepperValveCrntPin, 0);	// no current limit on valve so 2 amp
 }
+
+// M A I N   F U N C T I O N -- CREATE SQUARE ARRAY
+/*
+* Initializes the array of squares on startup
+* squareArray[squareID][{x,y,distance,angle}]
+* Each position in the first dimension of array is a given square id
+*
+*/
+
+void createSquareArray(int squaresPerRow)
+{
+	int squareID = 0;
+	int turretColumn = (squaresPerRow - 1) / 2;
+	int turretRow = turretColumn;
+
+	for (int row = 0; row < squaresPerRow; row++)
+	{
+		for (int column = 0; column < squaresPerRow; column++)
+		{
+			//Store col (x) and row (y) data for this square
+			squareArray[squareID][0] = column;
+			squareArray[squareID][1] = row;
+
+			// Calculates flow needed to reach this square, stores it in array
+			squareArray[squareID][2] = getFlow(column, row, turretColumn, turretRow);
+
+			// Calculates # of steps taken from home needed to make turret face square
+			squareArray[squareID][3] = convertAngleToStep(angleToSquare(column, row, turretColumn, turretRow));
+			Serial.println(squareArray[squareID][3]);
+
+			squareID += 1;
+			printf("Square id: %d\n", squareID);
+		}
+	}
+}
+
+// S U B   F U N C T I O N -- distance to square
+/*
+* Finds distance between two sets of x,y coordinates
+* Returns a double
+*/
+
+double distanceToSquare(int sqCol, int sqRow, int turCol, int turRow)
+{
+	int x = sqCol - turCol;
+	int y = sqRow - turRow;
+
+	int squareCoords = (x * x) + (y * y);
+
+	return sqrt((double)squareCoords);
+}
+
+// S U B   F U N C T I O N -- angleToSquare
+/*
+* Finds the angle to a given target square relative to a second square
+* give x/y coords, returns a double
+*/
+
+double angleToSquare(int sqCol, int sqRow, int turCol, int turRow)
+{
+	int x = sqCol - turCol;
+	int y = sqRow - turRow;
+	int quadrant = 0;
+
+	if (x != 0 && y != 0) {
+
+		if (getSign(x) == -1) {
+			if (getSign(y) == -1) {
+				quadrant = 4;
+			}
+			else {
+				quadrant = 3;
+			}
+		}
+		else {
+			if (getSign(y) == -1) {
+				quadrant = 1;
+			}
+			else {
+				quadrant = 2;
+			}
+		}
+
+	}
+	else {
+
+		quadrant = 0;
+
+		if (x == 0 && y == 0) {
+			return 999.0;
+		}
+		else if (x == 0) {
+			if (getSign(y) == -1) {
+				return 0.0;
+			}
+			else {
+				return 180.0;
+			}
+		}
+		else {
+			if (getSign(x) == -1) {
+				return 270.0;
+			}
+			else {
+				return 90.0;
+			}
+
+		}
+	}
+
+	double temp = ((double)abs(x) / (double)abs(y));
+
+	switch (quadrant) {
+	case 1: return (atan(temp) * 180) / PI; break;
+	case 2: return 180 - (atan(temp) * 180) / PI; break;
+	case 3: return 180 + (atan(temp) * 180) / PI; break;
+	case 4: return 360 - (atan(temp) * 180) / PI; break;
+	}
+}
+
+// S U B   F U N C T I O N -- convertAngleToStep
+/*
+* takes angle (as double) and returns the value as a number of steps (int).
+* Incorporates and rounds 3 sigfigs past the decimal point.
+*/
+
+int convertAngleToStep(double angle)
+{
+	double anglePerStep = 360.0 / STEPS_PER_FULL_TURN;  //Gets degrees of movement per step 
+
+	if (angle > 400)   //If the turret angle of 999 is found, return same number
+	{
+		return 999;
+	}
+
+	else
+	{
+		double stepsDouble = angle / anglePerStep;   //Get number of steps to reach this angle starting from home)
+		Serial.println(stepsDouble);
+		int steps = stepsDouble * 1000;
+
+		for (int i = 0; i < 3; i++)   //Rounds the number (including 3 sigfigs after the decimal point)
+		{
+			if (steps % 10 > 4)
+			{
+				steps = (steps / 10) + 1;
+			}
+			else
+			{
+				steps = steps / 10;
+			}
+		}
+		return steps;   //Return value;
+	}
+}
+
+// S U B   F U N C T I O N -- Get Flow
+/*
+*
+* Give a distance (x) and returns flow as int
+* Currently truncates flow value, but shouldn't be a problem as the system
+* isn't capable with that granular of a number in this case
+*/
+
+
+int getFlow(int column, int row, int turretColumn, int turretRow)
+{
+
+	double squareDistance = distanceToSquare(column, row, turretColumn, turretRow);   //Gets the distance from target square to the central turret square
+	double flow = 99857.81 - 23136.9*squareDistance + 1636.316*pow(squareDistance, 2); //Converts the distance value to flow (2nd-Order Polynomial)
+
+	return (int)flow;
+
+	// ** Other equations describing our curve **
+	// 4PL has the best R^2 value, but might be difficult to tweak.
+
+	//  4PL  
+	//  double y = 6944.746 + (94311.32 - 6944.746)/(1 + pow(x/2.473604,1.825548));
+
+	//  3rd-Order Polynomial 
+	//  double y = 107475.7 - 31263.28*x + 3826.97*pow(x,2) - 168.5722*pow(x,3);
+
+}
+
--- MainCode/MainCode/MainCode.ino
+++ MainCode/MainCode/MainCode.ino
@@ -4,9 +4,6 @@
 //  october 31, 2018
 
 
-//askdjhsadhiuhehfka error cannot computer test test
-
-
 // *********   P R E P R O C E S S O R S
 #include <Stepper.h>
 #include <BluetoothSerial.h>
@@ -31,8 +28,6 @@
 #define CCW -1
 #define CW  1
 
-
-// ********* P I N   A S S I G N M E N T S
 // flow meter
 #define pulsePin 23
 #define SAMPLES 4096
@@ -64,32 +59,6 @@
 #define solarPanelVoltage A7
 
 
-
-
-// ********    P R O T O T Y P E S
-
-
-void stepperOneStepHalfPeriod(byte x, byte y, byte z, int *q, int h);
-void stepperDomeOneStepHalfPeriod(int hf);
-void stepperValveOneStepHalfPeriod(int hf);
-void stepperGoHome(byte x, byte y, byte z, byte s);
-void domeGoHome();
-void valveGoHome();
-
-void stepperDomeDirCW();
-void stepperDomeDirCCW();
-
-void toggleStepperValveDir();
-void valveStepperOneStep();
-
-void getSerialData();
-void checkPacketNumber(char *singleSquareData[]);
-void checkChecksum(char *singleSquareData[]);
-int charToInt(char *thisChar, int thisCharLength);
-int getFlow(int column, int row, int turretColumn, int turretRow);
-double angleToSquare(int sqCol, int sqRow, int turCol, int turRow);
-int convertAngleToStep(double angle);
-
 void setup()
 {
 	initESP();  // Configures inputs and outputs/pin assignments, serial baud rate,
@@ -102,303 +71,88 @@ void setup()
 
 void loop()
 {
+	checkSystemState();
+}
 
-	//state machine
 
-	//interupt: if low battery from BMS -> state = lowPower
-	//interupt: if pushbutton depressed -> state = program
-	
+
+void checkSystemState()
+{
 	switch (systemState)
 	{
-		case sleeping:
-		{
-			
-			if (SerialBT.available() || Serial.available())
-			{
-				Serial.println("Changing to program State");
-				systemState = program;
+	case sleeping:
+	{
 
-			}
-	
-			break;
+		if (SerialBT.available() || Serial.available())
+		{
+			systemState = program;
 		}
 
-		case solar:
+		break;
+	}
+
+	case solar:
+	{
+
+		solarPowerTracker();
+
+
+		systemState = sleeping;
+
+		break;
+	}
+
+	case program:
+	{
+
+		getSerialData();
+
+
+
+		systemState = sleeping;
+
+		break;
+	}
+
+	case water:
+	{
+		//load correct instruction set for date and time
+		//reference temperature and apply modfifier to watering durations
+		//open thread for flow sensor
+		//run spray program
+		if (systemState_previous != systemState)
 		{
-			
-			solarPowerTracker();
-
-		
-			systemState = sleeping;
-
-			break;
+			Serial.printf("SystemState: Watering Mode");
 		}
 
-		case program:
+
+		systemState_previous = systemState;
+
+		break;
+	}
+
+	case low_power:
+	{
+		//close the valve
+		//set LED to red
+		//allow solar
+		//prevent water until battery > 50%
+		  //>50% -> perform last spray cycle
+		if (systemState_previous != systemState)
 		{
-			
-			getSerialData();
-			
-
-			
-			systemState = sleeping;
-
-			break;
+			Serial.printf("SystemState: Low Power Mode");
 		}
 
-		case water:
-		{
-			//load correct instruction set for date and time
-			//reference temperature and apply modfifier to watering durations
-			//open thread for flow sensor
-			//run spray program
-			if (systemState_previous != systemState)
-			{
-				Serial.printf("SystemState: Watering Mode");
-			}
+		systemState_previous = systemState;
 
-			
-			systemState_previous = systemState;
-
-			break;
-		}
-
-		case low_power:
-		{
-			//close the valve
-			//set LED to red
-			//allow solar
-			//prevent water until battery > 50%
-			  //>50% -> perform last spray cycle
-			if (systemState_previous != systemState)
-			{
-				Serial.printf("SystemState: Low Power Mode");
-			}
-
-			systemState_previous = systemState;
-
-			break;
-		}
+		break;
+	}
 	}
 }
-
-void realTimeClock()
-{
-
-}
-
-
-
-
-// M A I N F U N C T I O N  -- Get Serial Data
-/*
-*
-* Run in loop or state to look for incoming bluetooth serial data and handle it.
-*
-*/
-
-
-
-
-// M A I N  F U N C T I O N -- SHOOT SINGLE SQUARE
 
 void shootSingleSquare()
 {
 	int targetFlow = squareArray[getSquareID(singleSquareData)][2];
 	int targetStep = squareArray[getSquareID(singleSquareData)][3];
 
-	
-	
-	
-
 }
-
-
-// M A I N   F U N C T I O N -- CREATE SQUARE ARRAY
-/*
-* Initializes the array of squares on startup
-* squareArray[squareID][{x,y,distance,angle}]
-* Each position in the first dimension of array is a given square id
-*
-*/
-
-void createSquareArray(int squaresPerRow)
-{
-	int squareID = 0;
-	int turretColumn = (squaresPerRow - 1) / 2;
-	int turretRow = turretColumn;
-
-	for (int row = 0; row < squaresPerRow; row++)
-	{
-		for (int column = 0; column < squaresPerRow; column++)
-		{
-			//Store col (x) and row (y) data for this square
-			squareArray[squareID][0] = column;
-			squareArray[squareID][1] = row;
-
-			// Calculates flow needed to reach this square, stores it in array
-			squareArray[squareID][2] = getFlow(column, row, turretColumn, turretRow);
-
-			// Calculates # of steps taken from home needed to make turret face square
-			squareArray[squareID][3] = convertAngleToStep(angleToSquare(column, row, turretColumn, turretRow));
-			Serial.println(squareArray[squareID][3]);
-
-			squareID += 1;
-			printf("Square id: %d\n", squareID);
-		}
-	}
-}
-
-// S U B   F U N C T I O N -- distance to square
-/*
-* Finds distance between two sets of x,y coordinates
-* Returns a double
-*/
-
-double distanceToSquare(int sqCol, int sqRow, int turCol, int turRow)
-{
-	int x = sqCol - turCol;
-	int y = sqRow - turRow;
-
-	int squareCoords = (x * x) + (y * y);
-
-	return sqrt((double)squareCoords);
-}
-
-// S U B   F U N C T I O N -- angleToSquare
-/*
-* Finds the angle to a given target square relative to a second square
-* give x/y coords, returns a double
-*/
-
-double angleToSquare(int sqCol, int sqRow, int turCol, int turRow)
-{
-	int x = sqCol - turCol;
-	int y = sqRow - turRow;
-	int quadrant = 0;
-
-	if (x != 0 && y != 0) {
-
-		if (getSign(x) == -1) {
-			if (getSign(y) == -1) {
-				quadrant = 4;
-			}
-			else {
-				quadrant = 3;
-			}
-		}
-		else {
-			if (getSign(y) == -1) {
-				quadrant = 1;
-			}
-			else {
-				quadrant = 2;
-			}
-		}
-
-	}
-	else {
-
-		quadrant = 0;
-
-		if (x == 0 && y == 0) {
-			return 999.0;
-		}
-		else if (x == 0) {
-			if (getSign(y) == -1) {
-				return 0.0;
-			}
-			else {
-				return 180.0;
-			}
-		}
-		else {
-			if (getSign(x) == -1) {
-				return 270.0;
-			}
-			else {
-				return 90.0;
-			}
-
-		}
-	}
-
-	double temp = ((double)abs(x) / (double)abs(y));
-
-	switch (quadrant) {
-	case 1: return (atan(temp) * 180) / PI; break;
-	case 2: return 180 - (atan(temp) * 180) / PI; break;
-	case 3: return 180 + (atan(temp) * 180) / PI; break;
-	case 4: return 360 - (atan(temp) * 180) / PI; break;
-	}
-}
-
-// S U B   F U N C T I O N -- convertAngleToStep
-/*
-* takes angle (as double) and returns the value as a number of steps (int).
-* Incorporates and rounds 3 sigfigs past the decimal point.
-*/
-
-int convertAngleToStep(double angle)
-{
-	double anglePerStep = 360.0 / STEPS_PER_FULL_TURN;  //Gets degrees of movement per step 
-
-	if (angle > 400)   //If the turret angle of 999 is found, return same number
-	{
-		return 999;
-	}
-
-	else
-	{
-		double stepsDouble = angle / anglePerStep;   //Get number of steps to reach this angle starting from home)
-		Serial.println(stepsDouble);
-		int steps = stepsDouble * 1000;
-
-		for (int i = 0; i < 3; i++)   //Rounds the number (including 3 sigfigs after the decimal point)
-		{
-			if (steps % 10 > 4)
-			{
-				steps = (steps / 10) + 1;
-			}
-			else
-			{
-				steps = steps / 10;
-			}
-		}
-		return steps;   //Return value;
-	}
-}
-
-// S U B   F U N C T I O N -- Get Flow
-/*
-*
-* Give a distance (x) and returns flow as int
-* Currently truncates flow value, but shouldn't be a problem as the system
-* isn't capable with that granular of a number in this case
-*/
-
-
-int getFlow(int column, int row, int turretColumn, int turretRow)
-{
-
-	double squareDistance = distanceToSquare(column, row, turretColumn, turretRow);   //Gets the distance from target square to the central turret square
-	double flow = 99857.81 - 23136.9*squareDistance + 1636.316*pow(squareDistance, 2); //Converts the distance value to flow (2nd-Order Polynomial)
-
-	return (int)flow;
-
-	// ** Other equations describing our curve **
-	// 4PL has the best R^2 value, but might be difficult to tweak.
-
-	//  4PL  
-	//  double y = 6944.746 + (94311.32 - 6944.746)/(1 + pow(x/2.473604,1.825548));
-
-	//  3rd-Order Polynomial 
-	//  double y = 107475.7 - 31263.28*x + 3826.97*pow(x,2) - 168.5722*pow(x,3);
-
-}
-
-
-
-
-
-
